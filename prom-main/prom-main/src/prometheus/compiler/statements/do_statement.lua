574a9aac7a5262f9e2ac0d145362ed94c926a14d
axleoislost <jorgeitokramain@gmail.com>
2026-03-23 15:54:04 +0100

    Delete prom-main/prom-main/src directory

--- prom-main/prom-main/src/prometheus/compiler/statements/do_statement.lua
+++
@@ -1,9 +0,0 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- do_statement.lua
--- This Script contains the statement handler for the DoStatement
-
-return function(self, statement, funcDepth)
-    self:compileBlock(statement.body, funcDepth);
-end;
-
